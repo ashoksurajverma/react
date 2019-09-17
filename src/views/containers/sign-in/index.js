18e57926a7690d8e72630b5b71e2a09f8e7e41ed
Suraj Verma <ashoksurajverma@gmail.com>
2019-09-17 17:21:48 +0530

    Design sign-in page

--- src/views/containers/sign-in/index.js
+++ src/views/containers/sign-in/index.js
@@ -1,10 +1,41 @@
 import React, { Component } from "react";
+import './index.css';
 
 export default class SignIn extends Component {
   render() {
     return (
-      <div>
-        <h2> SignIn </h2>
+      <div className="container">
+        <div className="row">
+          <div className="col-md-12">
+            <form>
+              <div class="form-group">
+                <label for="exampleInputEmail1">Email address</label>
+                <input
+                  type="email"
+                  class="form-control"
+                  id="exampleInputEmail1"
+                  aria-describedby="emailHelp"
+                  placeholder="Enter email"
+                />
+                <small id="emailHelp" class="form-text text-muted">
+                  We'll never share your email with anyone else.
+                </small>
+              </div>
+              <div class="form-group">
+                <label for="exampleInputPassword1">Password</label>
+                <input
+                  type="password"
+                  class="form-control"
+                  id="exampleInputPassword1"
+                  placeholder="Password"
+                />
+              </div>
+              <button type="submit" class="btn btn-primary">
+                Submit
+              </button>
+            </form>
+          </div>
+        </div>
       </div>
     );
   }
