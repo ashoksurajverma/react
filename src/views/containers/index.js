91bc927b447ba7db5f01d897869d755fb78b2b57
Suraj Verma <ashoksurajverma@gmail.com>
2019-09-17 16:52:28 +0530

    configur react routing

--- src/views/containers/index.js
+++ src/views/containers/index.js
@@ -1,10 +1,24 @@
-import React from 'react';
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import SignIn from "./sign-in";
+import SignUp from "./sign-up";
+import NotFoundPage from './not-found-page';
+import Header from './header';
+import Footer from './footer';
 
 function App() {
   return (
-    <div className="App">
-      hello world
-    </div>
+    <Router>
+      <div>
+      <Header />
+      <Switch>
+        <Route exact path="/" component={SignIn} />
+        <Route path="/sign-up" component={SignUp} />
+        <Route path='*' exact={true} component={NotFoundPage} />
+      </Switch>
+      </div>
+      <Footer />
+    </Router>
   );
 }
 
