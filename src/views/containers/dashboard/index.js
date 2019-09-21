5f09eb56a0fadfa37462e5346837ac7b0de9f35e
Suraj Verma <ashoksurajverma@gmail.com>
2019-09-21 14:48:28 +0530

    Minor fixes on dashboard

--- src/views/containers/dashboard/index.js
+++ src/views/containers/dashboard/index.js
@@ -1,10 +1,12 @@
 import React, { Component } from "react";
+import './index.css';
 
 export default class Dashboard extends Component {
   render() {
     return (
-      <div>
-        <h2> This is Dashboard </h2>
+      <div className="dashboard">
+        <h2 className="dashboard-heading"> DASHBOARD </h2>
+        <p>Welcome to the <span>B-Fresh</span> Dashboard</p>
       </div>
     );
   }
