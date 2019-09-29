4d937f70935591de06e95eea1972ee94ae824b36
Suraj Verma <ashoksurajverma@gmail.com>
2019-09-29 14:26:09 +0530

    Add axios

--- src/state/ducks/sign-in/operations.js
+++ src/state/ducks/sign-in/operations.js
@@ -1,3 +1,5 @@
+import axios from 'axios';
+import qs from 'qs';
 import actions from "./actions";
 import { filter } from "lodash";
 
@@ -7,13 +9,65 @@ const mockData = [
 ];
 
 const logIn = data => {
+  debugger
+  const config = {
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8'
+    }
+  }
+  
+  const requestBody = {
+    user_object: {
+      email: 'avatar@gmail.com',
+      password: 'test123'
+    }
+  }
   return async dispatch => {
     // dispatch(actions.increaseCounter());
-    if (filter(mockData, data).length) {
-      dispatch(actions.setUserAuthenticated(true));
-      return true;
+    // if (filter(mockData, data).length) {
+    //   dispatch(actions.setUserAuthenticated(true));
+    //   return true;
+    // }
+    // return false;
+    // fetch('http://avatartechworld.com/rest/auth/org_login', {
+    //   method: 'POST',
+    //   headers: {
+    //     'Accept': 'application/json',
+    //     'Content-Type': 'application/x-www-form-urlencoded',
+    //     'mode': 'no-cors'
+    //   },
+    //   body: requestBody
+    // }).then((response) => {
+    //   debugger
+    // })
+    // .catch((error) => {
+    //   debugger
+    // })
+    const url = 'http://avatartechworld.com/rest/auth/org_login';
+
+    const options = {
+      method: 'POST',
+      headers: { 'content-type': 'application/x-www-form-urlencoded' },
+      data: qs.stringify(requestBody),
+      url
     }
-    return false;
+    axios(options).then((res) => {
+      // debugger
+    })
+    .catch(error => {
+      // debugger
+    })
+    // axios.post('http://avatartechworld.com/rest/auth/org_login', config, qs.stringify(requestBody))
+    // .then(function (response) {
+    //   debugger
+    //   // handle success
+    //   console.log(response);
+    // })
+    // .catch(function (error) {
+    //   debugger
+    //   // handle error
+    //   console.log(error);
+    // })
   };
 };
 const logOut = () => {
